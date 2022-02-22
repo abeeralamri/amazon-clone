52512274a5c1901c3efaf2eec34383028bc5ac80
abeeralamri <abeer.alamri1997@gmail.com>
2022-02-22 19:11:57 +0300

    implemented devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   resources :items do 
     member do 
       delete :delete_pic
@@ -9,7 +11,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#home"
   get '/items/:id/delete_pic', to: 'items#delete_pic', as: :DELETE
   get '/items/:id/delete_cover', to: 'items#delete_cover'
+  get 'home', to: "application#home"
 end
